8da8856c4fa8f98dcf0a9c462e4eb60f6ffc8c04
TanPhoi <devtanphoi@gmail.com>
2024-05-12 22:11:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+const navPc = document.querySelector("#menu-drawer__navbar-list--pc");
+const navMobile = document.querySelector("#menu-drawer__navbar-list--mobile");
+navMobile.innerHTML = navPc.innerHTML;
